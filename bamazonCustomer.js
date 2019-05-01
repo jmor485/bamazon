c3f227c6c3c53dff833ebf8eed5511636b09c7c9
Julie Morrison <jmor485@gmail.com>
2019-04-30 23:47:22 -0400

    first commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,80 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazonDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+var showProducts = function () {
+    var query = "Select * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        var displayTable = new Table({
+            head: ["Item ID", "Product Name", "Department Name", "Price", "Stock Quantity"],
+            colWidths: [10, 25, 25, 10, 10]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        userPrompt();
+    });
+
+
+    function userPrompt() {
+
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "What is the item ID of the product you wish to purchase?",
+                filter: Number
+            },
+            {
+                type: "input",
+                name: "quantitiy",
+                message: "How many units of the product would you like to buy?",
+                filter: Number
+            },
+
+        ]).then(function (answers) {
+
+            var desiredQuantity = answers.quantity;
+            var productID = answers.id;
+            purchaseProducts(productID, desiredQuantity);
+
+        });
+    };
+
+    function puchaseProducts(id, restockAmt) {
+        connection.query("Select * FROM products WHERE item_id = " + id, function (err, res) {
+            if (err) { console.log(err) };
+
+            if (restockAmt <= res[0].stock_quantity) {
+                var totalCost = res[0].price * restockAmt;
+                console.log("YAY! Your order is in stock!");
+                console.log("Your total cost for " + restockAmt + " "
+                    + res[0].product_name + " is " + totalCost + " Thank you!");
+
+
+                connection.query("UPDATE products SET stock_quantity = stock_quantity - " + restockAmt + "WHERE item_id = " + id);
+            }
+            else {
+                console.log("OH NO! Insufficient quantity... " + res[0].product_name + "try again.");
+            };
+            showProducts();
+        });
+    };
+    showProducts();
+}
---
+++ myschema.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazonDB;
+USE bamazonDB;
+
+CREATE TABLE products (
+item_id INTEGER (5) NOT NULL,
+product_name VARCHAR (100),
+department_name VARCHAR (100),
+price DECIMAL (10,4),
+stock_quantity INTEGER (10),
+PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (4214, "Instant Pot Pressure Cooker", "Home & Kitchen", 49.99, 1500);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (5961, "Gel Enhanced Seat Cushion", "Office", 22.65, 4020);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (4981, "Handheld Milk Frother", "Home & Kitchen", 10.10, 730);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (5286, "Youth Kids Swim Goggles", "Sports & Outdoors", 10.00, 1040);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3969, "Noise Cancelling Headphones", "Electronics", 49.99, 610);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (4252, "Natural Hemp Gummies", "Health & Household", 29.70, 289);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3565, "Waterproof Bluetooth Headphones", "Electronics", 18.95, 3000);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (4875, "Vitamin C Face Serum", "Beauty & Personal Care", 7.99, 2200);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3217, "Indoor Pet Camera", "Pet Supplies", 43.99, 555);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (5692, "Lightweight Mesh Running Shoes", "Clothing, Shoes & Jewelry", 39.99, 1380);
